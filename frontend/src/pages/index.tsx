0a0fa49a24c506e907d07d72602ba5bd7b112943
harambasha <ismirharambasic@gmail.com>
2025-06-09 14:52:07 +0200

    feat: update index page

--- frontend/src/pages/index.tsx
+++ frontend/src/pages/index.tsx
@@ -3,9 +3,7 @@ import { Wizard } from '../components/Wizard';
 export default function Home() {
   return (
     <main className="min-h-screen bg-gradient-to-b from-gray-50 to-white">
-      <div className="container mx-auto px-4 py-8">
         <Wizard />
-      </div>
     </main>
   );
 } 
